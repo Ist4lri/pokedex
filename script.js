538761e5e15810024e6c366c5b29ace0121f74f9
Ist4lri <pierreloic.gouttebel@gmail.com>
2022-07-21 20:42:52 +0200

    Projet a peu près finalisé

--- script.js
+++ script.js
@@ -1,29 +1,44 @@
-document
-    .querySelector("input[id='requestPokemon']")
-    .addEventListener('keypress', function(e){
-            var userEntry = e.target.value;
+function main() {
+
+    const pokemonNameElement = document.getElementById("requestPokemon");
+
+    const bla = document.getElementById("name");
+
+    const selectorButton = document.getElementById("searchPokemon");
+
+    selectorButton.addEventListener('click', async function(event){
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon/" + pokemonNameElement.value);
+        if(response.ok === false) {
+            // handle errors here
+            throw new Error("Ce pokémon n'existe pas");
+        }
+        const pokemon = await response.json();
+        document
+            .getElementById("name")
+            .innerText = "Nom : " + pokemon.name;
+        document
+            .getElementById("weight")
+            .innerText = "Il fait " + pokemon.weight + "g";
+        document
+            .getElementById("type")
+            .innerText = "Il est de type(s) " + pokemon.types[0].type.name;
+
+        pokemon.types.map((oneType) => {
+            console.log(`${pokemon.name} est de type ${oneType.type.name}`);
+        })
+        
+        console.log({pokemon});
     });
+}
 
-
-document
-.querySelector("input[id='button']")
-.addEventListener('submit', function(event){
-    event.defaultPrevented();
-    document.getElementById("name").innerText = userEntry;
+document.addEventListener("DOMContentLoaded", function(event) {
+    try {
+        main();
+    } catch(e) {
+        document
+              .getElementById("weight")
+             .innerText = "Oula y'a un soucis !"
+    }
 });
-
-fetch("https://pokeapi.co/api/v2/pokemon/" + userEntry)
-.then(function(value){
-    document
-        .getElementById("name")
-        .innerText = value.name;
-    document
-        .getElementById("type")
-        .innerText = value.types[0].type.name;
-    document
-        .getElementById("weight")
-        .innerText = value.weight + "g";
-})
-
 
   
