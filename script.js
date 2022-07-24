da55f458bd695ff9ad8f7d1bbee1764276c92c83
Ist4lri <pierreloic.gouttebel@gmail.com>
2022-07-24 16:45:08 +0200

    Forme finale. Ajout de QueryString en cours

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-function buttonDisable(condition){
-    document
-        .getElementById('searchPokemon')
-        .disabled = condition;
+function disableButton(option){
+    document.getElementById("searchPokemon").disabled = option;
 }
 
 function main() {
 
-    buttonDisable(true);
-
     const pokemonNameElement = document.getElementById("requestPokemon");
 
-    document.getElementById("requestPokemon").addEventListener('keypress', buttonDisable(false));
+    document.getElementById("requestPokemon").addEventListener('input', function(e){
+        if(/^[A-Za-z]{3,}$/.test(e.target.value) === false){
+            disableButton(true);
+        } else {
+            disableButton(false);
+        }
+    });
 
    document
         .getElementById("searchPokemon")
@@ -19,14 +21,20 @@ function main() {
             const response = await fetch("https://pokeapi.co/api/v2/pokemon/" + pokemonNameElement.value);
            
             if(response.ok === false) {
-
                 document
                     .getElementById("exist")
                     .innerHTML = "<strong>Ce pokémon n'existe pas. Ou il est écrit en français...</strong>";
+            } else {
+                document
+                    .getElementById("exist")
+                    .innerHTML = "";
             }
 
             const pokemon = await response.json();
-            
+
+            document
+                .getElementById("image")
+                .setAttribute("src", pokemon.sprites.front_default);
             document
                 .getElementById("name")
                 .innerText = "Nom : " + pokemon.name;
