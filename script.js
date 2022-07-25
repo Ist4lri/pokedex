f444e414bc43063477f1493565aaf26a1204f261
Ist4lri <pierreloic.gouttebel@gmail.com>
2022-07-25 15:15:19 +0200

    Ajout de la fonctionnalité QS. Pas prore.

--- script.js
+++ script.js
@@ -2,6 +2,49 @@ function disableButton(option){
     document.getElementById("searchPokemon").disabled = option;
 }
 
+async function stringQuery(){
+    const isQueryString = window.location.href.indexOf('?');
+    
+    if (isQueryString !== -1){
+        
+        const queryString = window.location.href.substring(isQueryString + 1);
+        var valuePokemon = new URLSearchParams(queryString).get("value");
+
+        console.log({valuePokemon});
+        
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon/" + valuePokemon);
+           
+            if(response.ok === false) {
+                document
+                    .getElementById("exist")
+                    .innerHTML = "<strong>Ce pokémon n'existe pas. Ou il est écrit en français...</strong>";
+            } else {
+                document
+                    .getElementById("exist")
+                    .innerHTML = "";
+            }
+
+            const pokemon = await response.json();
+
+            document
+                .getElementById("image")
+                .setAttribute("src", pokemon.sprites.front_default);
+            document
+                .getElementById("name")
+                .innerText = "Nom : " + pokemon.name;
+            document
+                .getElementById("weight")
+                .innerText = "Il fait " + pokemon.weight + "g";
+            document
+                .getElementById("type")
+                .innerText = "Il est de type(s) " + pokemon.types.map((singleType) => singleType.type.name + " ")
+            document
+                .getElementById("pokeId")
+                .innerText = "ID du pokémon : " + pokemon.id;
+    }
+
+}
+
 function main() {
 
 
@@ -44,11 +87,15 @@ function main() {
             document
                 .getElementById("type")
                 .innerText = "Il est de type(s) " + pokemon.types.map((singleType) => singleType.type.name + " ")
+            document
+                .getElementById("pokeId")
+                .innerText = "ID du pokémon : " + pokemon.id;
         
     });
 }
 
 document.addEventListener("DOMContentLoaded", function(event) {
+    stringQuery();
     main();    
 });
 
