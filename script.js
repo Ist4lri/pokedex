1231360c103da1bc54d838edec26143597b6e65d
Ist4lri <pierreloic.gouttebel@gmail.com>
2022-07-25 16:57:48 +0200

    optimisation

--- script.js
+++ script.js
@@ -1,26 +1,21 @@
 function disableButton(){
-    if(/^[A-Za-z]{3,}$/.test(e.target.value) !== false){
-        document.getElementById("searchPokemon").disabled = false;
+    console.log("+1");
+    if(/^[A-Za-z]{3,}$/.test(document.getElementById("requestPokemon")) !== false){
+        document.getElementById("searchPokemon").disableButton = true;
     }
 }
-function testIfError(params) {
 
-    if (params.ok === false) {
+async function fetchPokemon(pokemonNameOrId){
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon/" + pokemonNameOrId);
+    if (response.ok === false) { 
         throw new Error("Ce pokémon n'existe pas");
     }
+    var pokemon = await response.json();
 
+    return pokemon;
 }
-async function resultAPI(params){
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon/" + params);
 
-            testIfError(response);
-            
-            return await response.json();
-}
 function updatePokedex(pokemon){
-    document
-    .getElementById("image")
-    .setAttribute("src", pokemon.sprites.front_default);
 document
     .getElementById("name")
     .innerText = "Nom : " + pokemon.name;
@@ -33,33 +28,33 @@ document
 document
     .getElementById("pokeId")
     .innerText = "ID du pokémon : " + pokemon.id;
+document
+    .getElementById("image")
+    .setAttribute("src", pokemon.sprites.front_default);
 }
 
 function stringQuery(){
     const isQueryString = window.location.href.indexOf('?'); // (URL)
     
-    if (isQueryString === -1){ // faire l'inverse
-        
-        return "undefined"; // a faire en fonction
+    if (isQueryString === -1){
+        return undefined;
     }
 
     const queryString = window.location.href.substring(isQueryString + 1); //supr ça et balancer directement dans le .get()
-        var valuePokemon = new URLSearchParams(queryString).get("value");
+    var valuePokemon = new URLSearchParams(queryString).get("value");
 
         console.log({valuePokemon});
 
 } 
 
 function main() {
-
-
     const pokemonNameElement = document.getElementById("requestPokemon");
-
-    document.getElementById("requestPokemon").addEventListener('input', disableButton());
-        
-    document.getElementById("searchPokemon").addEventListener('click', updatePokedex(resultAPI(pokemonNameElement.value)){
-        
-});
+    document.getElementById("requestPokemon").addEventListener('keypress', (event) => disableButton());
+    document.getElementById("searchPokemon").addEventListener('click', async (event) => {
+        const pokemon = await fetchPokemon(pokemonNameElement.value);
+        updatePokedex(pokemon);
+    });
+}
 
 document.addEventListener("DOMContentLoaded", function(event) {
     stringQuery();
